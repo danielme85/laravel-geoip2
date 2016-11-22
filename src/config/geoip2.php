287134313c91b10f0ee0836b49fd4d4a21395267
Daniel Mellum <mellum@gmail.com>
2016-11-22 10:17:31 -0500

    error in configfile

--- src/config/geoip2.php
+++ src/config/geoip2.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-    'geoip2' => [
-        'downloadUrl' => 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz', //url db file download
-        'tempFile' => 'app/GeoLite2-City.mmdb.gz', //temp download file name
-        'dbName' => 'app/GeoLite2-City.mmdb', //Geoip DB filename
-        'localhost' => '8.8.8.8' //when running on localhost (or for general testing) you can specify a fake ip address here.
-    ]
+    'downloadUrl' => 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz', //url db file download
+    'tempFile' => 'app/GeoLite2-City.mmdb.gz', //temp download file name
+    'dbName' => 'app/GeoLite2-City.mmdb', //Geoip DB filename
+    'localhost' => '8.8.8.8' //when running on localhost (or for general testing) you can specify a fake ip address here.
 ];
